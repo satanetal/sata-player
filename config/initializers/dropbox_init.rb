7824f25903b8e69cfab770b781e9afcee4f1f45a
satanetal <dubstepkillah@gmail.com>
2017-10-03 17:50:14 +0300

    deleted config for ouath v1 dropbox

--- config/initializers/dropbox_init.rb
+++
@@ -1,6 +0,0 @@
-Dropbox::API::Config.app_key    = "nazu7wtht0lkbwf"
-Dropbox::API::Config.app_secret = "k6le6hnq9ul5uuy"
-# Dropbox::API::Config.app_key    = YOUR_APP_KEY
-# Dropbox::API::Config.app_secret = YOUR_APP_SECRET
-# Dropbox::API::Config.mode       = "sandbox" # if you have a single-directory app
-Dropbox::API::Config.mode       = "dropbox" # if your app has access to the whole dropbox
